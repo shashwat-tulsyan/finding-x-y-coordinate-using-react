538da556980a183b1f5c38f173a8822d7e2928f7
SHASHWAT TULSYAN <shashwattulsyan123@gmail.com>
2022-04-07 11:15:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-//import Classcom from './classcom';
+import Classcom from './classcom';
 import Funcom from './funcom';
 function App() {
   return (
     <>
-    {/* <Classcom/> */}
+     <Classcom/> 
     <Funcom/>
     </>
   );
